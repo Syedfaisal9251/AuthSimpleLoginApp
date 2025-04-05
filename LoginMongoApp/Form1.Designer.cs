efee2d435610620ea7948743b17e3476810b8c5d
Syed Faisal <syedfaisal9251@gmail.com>
2025-04-05 14:03:15 +0530

    Add project files.

---
+++ LoginMongoApp/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace LoginMongoApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblEmail.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lblEmail.Location = new System.Drawing.Point(115, 87);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(50, 22);
+            this.lblEmail.TabIndex = 0;
+            this.lblEmail.Text = "Email";
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtEmail.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtEmail.Location = new System.Drawing.Point(230, 87);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(230, 26);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.lblPassword.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblPassword.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.lblPassword.Location = new System.Drawing.Point(111, 146);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(78, 20);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.txtPassword.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtPassword.Location = new System.Drawing.Point(230, 143);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(230, 26);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.Olive;
+            this.btnLogin.ForeColor = System.Drawing.SystemColors.Info;
+            this.btnLogin.Location = new System.Drawing.Point(114, 220);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 37);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(226, 228);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 20);
+            this.lblStatus.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Violet;
+            this.ClientSize = new System.Drawing.Size(836, 450);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblEmail);
+            this.Name = "Form1";
+            this.Text = "LOGIN FORM";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ LoginMongoApp/Form1.cs
@@ -0,0 +1,102 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginMongoApp
+{
+
+    public partial class Form1 : Form
+    {
+        private IMongoCollection<BsonDocument> userCollection;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // MongoDB connection initialization
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("AuthDB");
+            userCollection = database.GetCollection<BsonDocument>("Users");
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            string email = txtEmail.Text.Trim();
+            string password = txtPassword.Text;
+
+           
+            if (!IsValidEmail(email))
+            {
+                lblStatus.Text = "Invalid email format.";
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+                return;
+            }
+
+        
+            if (!IsStrongPassword(password))
+            {
+                lblStatus.Text = "Password must be at least 8 characters, include upper & lower case letters, a digit, and a special character.";
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+                return;
+            }
+
+           
+            var filter = Builders<BsonDocument>.Filter.Eq("email", email) &
+                         Builders<BsonDocument>.Filter.Eq("password", password);
+
+            var user = userCollection.Find(filter).FirstOrDefault();
+
+            if (user != null)
+            {
+                lblStatus.Text = "Login successful!";
+                lblStatus.ForeColor = System.Drawing.Color.Green;
+                MessageBox.Show("Welcome, " + email);
+            }
+            else
+            {
+                lblStatus.Text = "Email or Password cannot be empty.";
+                lblStatus.ForeColor = System.Drawing.Color.Red;
+            }
+
+        }
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+       
+        private bool IsStrongPassword(string password)
+        {
+            if (password.Length < 8)
+                return false;
+
+            bool hasUpper = false, hasLower = false, hasDigit = false, hasSpecial = false;
+
+            foreach (char c in password)
+            {
+                if (char.IsUpper(c)) hasUpper = true;
+                else if (char.IsLower(c)) hasLower = true;
+                else if (char.IsDigit(c)) hasDigit = true;
+                else if (!char.IsLetterOrDigit(c)) hasSpecial = true;
+            }
+
+            return hasUpper && hasLower && hasDigit && hasSpecial;
+        }
+    }
+}
